b20088a245c835684a12e72a46f2f23f674883e2
Aris Fergadis <aris.fergadis@protonmail.com>
2017-06-25 02:10:27 +0300

    Trend collection runs a scheduled event within application.

--- src/main/java/gr/ntua/domain/Trend.java
+++ src/main/java/gr/ntua/domain/Trend.java
@@ -1,17 +1,16 @@
 package gr.ntua.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by aris on 13/6/2017.
  */
 @Entity
-@Table(name = "trends_backup")
+@Table(name = "trends_new")
 public class Trend {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
     @Column(name = "timespan_id")
@@ -24,6 +23,22 @@ public class Trend {
     private Double bursting;
     @Column(name = "first_seen")
     private boolean firstSeen;
+    @Column(name = "date_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateTime;
+
+    public Trend(Long timespanId, String name, Integer volume) {
+        this.timespanId = timespanId;
+        this.name = name;
+        this.volume = volume;
+        bursting = 0.0;
+        firstSeen = false;
+        dateTime = new Date();
+    }
+
+    public Trend() {
+
+    }
 
     public Long getId() {
         return id;
@@ -73,6 +88,14 @@ public class Trend {
         this.firstSeen = firstSeen;
     }
 
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
     @Override
     public String toString() {
         return "Trend{" +
@@ -82,6 +105,8 @@ public class Trend {
                 ", volume=" + volume +
                 ", bursting=" + bursting +
                 ", firstSeen=" + firstSeen +
+                ", dateTime=" + dateTime +
                 '}';
     }
+
 }
